21ec1bca07cfd114fa9e514a9992ebc8f8260943
Will Weindel <will.weindel@gmail.com>
2021-07-26 18:30:59 -0700

    adds IOCE + DA/P

--- Ch 1 - Arrays and Strings/1-isUnique/isUnique.js
+++ Ch 1 - Arrays and Strings/1-isUnique/isUnique.js
@@ -1,7 +1,18 @@
 //Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
+//I - a string
+//O - boolean
+//C - greater than 26 letters? (only if single word)
+//E - spaces? empty strings?
+//D - HashTable or object with key
+//A/P - double for loop
+
 function isUnique(str) {
-  //code
+  for (var char of str) {
+    for (var i = 0; i < str.length; i++) {
+
+    }
+  }
 }
 
 module.exports = isUnique;
