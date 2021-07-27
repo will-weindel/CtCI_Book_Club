867456f7133e4c7bf9d04e7ea1aae65f4548e45d
Will Weindel <will.weindel@gmail.com>
2021-07-26 18:52:36 -0700

    adds IOCE DA/P to checkPermutations

--- Ch 1 - Arrays and Strings/2-checkPermutations/checkPermutations.js
+++ Ch 1 - Arrays and Strings/2-checkPermutations/checkPermutations.js
@@ -1,5 +1,13 @@
 //Given two strings, write a method to decide if one is a permutation of the other.
 
+//I - two strings
+//O - bool
+//C - check for other data types, strs of diff length
+//E - spaces? empty strings
+
+//D - hash / obj
+//A/P - linear (using sucessive for loops)
+
 const checkPermutations = (str1, str2) => {
   //Please code here
 };
